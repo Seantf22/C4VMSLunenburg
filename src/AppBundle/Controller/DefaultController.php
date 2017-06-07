354c806496bba1ed1c8c0bdd4be15d21d866166a
Ferbske <rick.voesenek@hotmail.com>
2017-06-07 11:26:49 +0200

    Inloggen gaat naar de nieuwe homepage + lorem ipsum

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,22 +9,11 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
-    }
-
-    /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function home(Request $request)
     {
-        return $this->render('base.html.twig', [
+        return $this->render('home.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
